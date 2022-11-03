08bc0890d3179f440961703d439bc39a43b49b3b
Alexander Vyzhanov <lembdev@gmail.com>
2022-11-03 15:38:49 +0200

    ci: `lint-staged` invalid config

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -6,19 +6,15 @@ const removeIgnoredFiles = async (files) => {
     files.map((file) => eslint.isPathIgnored(file)),
   );
 
-  return files
-    .filter((_, i) => !isIgnored[i])
-    .filter((file) => !file.endsWith('.hbs'))
-    .join(' ');
+  return files.filter((_, i) => !isIgnored[i]).join(' ');
 };
 
 module.exports = {
-  '*': async (files) => {
-    const filtredFiles = await removeIgnoredFiles(files);
-    return [`prettier --ignore-unknown --write ${filtredFiles}`];
-  },
   '**/*.ts': async (files) => {
     const filtredFiles = await removeIgnoredFiles(files);
-    return [`eslint --max-warnings=0 ${filtredFiles}`];
+    return [
+      `prettier --ignore-unknown --write  ${files}`,
+      `eslint --max-warnings=0 ${filtredFiles}`,
+    ];
   },
 };
